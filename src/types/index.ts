f96d7cdd9909147f73c0895ca1009f9fac0a6b2f
autism <609886059@qq.com>
2026-03-01 23:52:37 +0800

    feat: 添加数据类型定义

---
+++ src/types/index.ts
@@ -0,0 +1,41 @@
+export interface MemoryRegion {
+  name: string;
+  origin: number;
+  length: number;
+  used: number;
+  free: number;
+}
+
+export interface Symbol {
+  name: string;
+  address: number;
+  size: number;
+  section: string;
+  file: string;
+}
+
+export interface Module {
+  name: string;
+  totalSize: number;
+  textSize: number;
+  dataSize: number;
+  bssSize: number;
+}
+
+export interface ParsedMapData {
+  memoryRegions: MemoryRegion[];
+  symbols: Symbol[];
+  modules: Module[];
+}
+
+export interface AlignmentIssue {
+  symbolName: string;
+  address: number;
+  wastedBytes: number;
+}
+
+export interface OptimizationSuggestion {
+  alignmentIssues: AlignmentIssue[];
+  unusedSymbols: string[];
+  duplicateSymbols: string[];
+}
